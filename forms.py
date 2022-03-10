601f107c0ce7b9f60af7b52b4f4d53a5d30aee78
Fredrik Skorstol <fredrik_skorstol@hotmail.com>
2022-03-10 22:27:54 +0100

    Some fixup

--- forms.py
+++ forms.py
@@ -12,3 +12,9 @@ class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+class DashboardForm():
+    number = None
+    new_number = None
+    tools = []
+    expname = []
--- static/js/tablefilter.js
+++ static/js/tablefilter.js
@@ -18,36 +18,31 @@ function SortByName() {
     }
   }
 
-//sort table by tools
 function SortByTools(newId){
     var table, tr, td, i, txtValue;
     var id_vid = document.getElementById("drop-vid");
     var id_pict = document.getElementById("drop-pict");
-    var id_eeg = document.getElementById("drop-eeg");
     var id_analyt = document.getElementById("drop-analyt");
-    var id_sens = document.getElementById("drop-sens");
     var id_time = document.getElementById("drop-time");
     var id_cardio = document.getElementById("drop-cardio");
     var id_quest = document.getElementById("drop-quest");
     
     var filter1 = id_vid.value.toUpperCase();
     var filter2 = id_pict.value.toUpperCase();
-    var filter3 = id_eeg.value.toUpperCase();
-    var filter4 = id_analyt.value.toUpperCase();
-    var filter5 = id_sens.value.toUpperCase();
-    var filter6 = id_time.value.toUpperCase();
-    var filter7 = id_cardio.value.toUpperCase();
-    var filter8 = id_quest.value.toUpperCase();
+    var filter3 = id_analyt.value.toUpperCase();
+    var filter4 = id_time.value.toUpperCase();
+    var filter5 = id_cardio.value.toUpperCase();
+    var filter6 = id_quest.value.toUpperCase();
 
     table = document.getElementById("DataTab");
     tr = table.getElementsByTagName("tr");
 
     $(document).one('click', '.dropdown-menu button', function() {
         newId = $(this).text().toUpperCase();
-        //alert(newId +","+ filter4);
         for (i = 0; i < tr.length; i++) 
         {
             td = tr[i].getElementsByTagName("td")[1];
+            //alert(newId +","+ filter4 +","+ td);
             if (td && newId == filter1) 
             {
                 txtValue = td.textContent || td.innerText;
@@ -114,28 +109,6 @@ function SortByTools(newId){
                     tr[i].style.display = "none";
                 }
             }
-            if (td && newId == filter7) 
-            {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter7) > -1) {
-                    tr[i].style.display = "";
-                }
-                else 
-                {
-                    tr[i].style.display = "none";
-                }
-            }
-            if (td && newId == filter8) 
-            {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter8) > -1) {
-                    tr[i].style.display = "";
-                }
-                else 
-                {
-                    tr[i].style.display = "none";
-                }
-            }
         }
         
     });
@@ -157,8 +130,6 @@ function ClearTools(){
         }
     }
 }
-
-//sort table by title
 
 function SortByTitle() {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
@@ -197,19 +168,19 @@ function SortByTitle() {
     }
 }
 
-
-//sorting table by date, both asc and desc.
 var toggle = true;
 
 function ConvertDate(y) {
-    var x = y.split("/");
-    return +(x[2]+x[1]+x[0]);
+    var first = y.split(" ")
+    var date = first[0].split('-')
+    var time = first[1].split(':')
+    return +(date[2]+date[1]+date[0]+time[0]+time[1]+time[2]);
   }
 
 function SortByDateAsc() {
     var tbody = document.querySelector("#DataTab tbody");
     var rows = [].slice.call(tbody.querySelectorAll("tr"));
-    
+
     rows.sort(function(a,b) {
         var asc = ConvertDate(b.cells[0].innerHTML) - ConvertDate(a.cells[0].innerHTML);
         return asc;
@@ -242,3 +213,51 @@ function toggleSort() {
 }
 
 document.querySelector("datebtn").addEventListener("click", toggleSort);
+
+var toggleR = true;
+
+function ShowRecent() {
+    var tbody = document.querySelector("#DataTab tbody");
+    var rows = [].slice.call(tbody.querySelectorAll("tr"));
+    
+    rows.sort(function(a,b) {
+        var asc = ConvertDate(b.cells[0].innerHTML) - ConvertDate(a.cells[0].innerHTML);
+        return asc;
+    });
+
+    rows.forEach(function(v) {
+        tbody.appendChild(v);
+    });
+
+    for (i = 1; i < rows.length; i++) 
+    {
+        if (i < 3) 
+        {
+            rows[i].style.display = "";
+        }
+        else {
+            rows[i].style.display = "none";
+        }
+    }
+    toggleR = true;
+}
+
+function DisableRecent() {
+    table = document.getElementById("DataTab");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) 
+    {
+        td = tr[i].getElementsByTagName("th")[0];
+        if (td) 
+        {
+            tr[i].style.display = "";
+        }
+    }
+    toggleR = false;
+}
+
+function toggleRecent() {
+    toggleR ? DisableRecent() : ShowRecent();
+}
+
+document.querySelector("recentbtn").addEventListener("click", toggleRecent);
