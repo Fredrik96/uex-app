66e09ddcd319904be8b12df448f6b1e80ea728a6
Fredrik Skorstol <fredrik_skorstol@hotmail.com>
2022-05-30 13:12:02 +0200

    tweeked app for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,8 @@ def create_app(config):
     bootstrap = Bootstrap(app)
     migrate = Migrate(app, db)
     moment = Moment(app)
-    app.config.from_object(config)
+    #app.config.from_object(config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pkkfxybimfvdff:26cbe64d988bc53e68f254d61689ca142401bc867d2446c25c69ab33eec79c4f@ec2-44-194-117-205.compute-1.amazonaws.com:5432/d6gujdtsf5l8ce'
     config.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
