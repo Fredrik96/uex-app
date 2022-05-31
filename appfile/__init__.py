44579691ae8cdefe92f994c08988a301add9af9c
Fredrik Skorstol <fredrik_skorstol@hotmail.com>
2022-05-31 11:46:53 +0200

    fixup

--- appfile/__init__.py
+++ appfile/__init__.py
@@ -10,8 +10,9 @@ from sqlalchemy import MetaData
 import os
 
 from video import Camera
-from process import webopencv 
-camera = Camera(webopencv())
+from process import webopencv
+import logging
+from sys import stdout
 
 convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -26,6 +27,8 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 login_manager = LoginManager()
 
+camera = Camera(webopencv())
+
 def create_app(config):
     app = Flask(__name__)
     bootstrap = Bootstrap(app)
@@ -35,6 +38,8 @@ def create_app(config):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pkkfxybimfvdff:26cbe64d988bc53e68f254d61689ca142401bc867d2446c25c69ab33eec79c4f@ec2-44-194-117-205.compute-1.amazonaws.com:5432/d6gujdtsf5l8ce'
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
+    app.logger.addHandler(logging.StreamHandler(stdout))
+    app.config['DEBUG'] = True
     socketio = SocketIO(app, logger=True)
     config.init_app(app)
     db.init_app(app)
@@ -50,6 +55,10 @@ def create_app(config):
     def test_message(input):
         input = input.split(",")[1]
         camera.enqueue_input(input)
+
+    @socketio.on('connect', namespace='/test')
+    def test_connect():
+        app.logger.info("client connected")
 
     return app
     
